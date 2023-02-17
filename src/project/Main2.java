4454eed6655ccd888824c71dde1f32515dee3f1d
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-02-17 21:42:27 +0700

    Edit

--- src/project/Main2.java
+++ src/project/Main2.java
@@ -1,10 +1,13 @@
 package project;
 
+import java.util.Iterator;
+
 public class Main2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2");
+		
 
 	}
 
