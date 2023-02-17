d375a946ea37756bff3f9cdf089c6183a6263104
Flower Garden <scome007@gmail.com>
2023-02-17 21:25:09 +0700

    Lilytest

--- src/project/Main.java
+++ src/project/Main.java
@@ -5,7 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+<<<<<<< Updated upstream
 		System.out.println("ART");
+=======
+		System.out.println("Test");
+		for(int i = 0;i<10;i++) {
+			System.out.println("kuy");
+		}
+>>>>>>> Stashed changes
 
 	}
 
