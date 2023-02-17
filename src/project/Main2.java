d6763ffdfaceb85448d3a9362d4ce206041bfa51
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-02-17 21:48:26 +0700

    Update Main2.java

--- src/project/Main2.java
+++ src/project/Main2.java
@@ -5,13 +5,8 @@ import java.util.Iterator;
 public class Main2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		/*System.out.println("2");*/
-=======
 		System.out.println("2");
-		
->>>>>>> 4454eed6655ccd888824c71dde1f32515dee3f1d
+		System.out.println("lolo");
 
 	}
 
