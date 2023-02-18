2ba617640c9f8df6b3454526854430f047d48e7c
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-02-18 14:31:11 +0700

    Just Created UI

--- src/project/Main.java
+++ src/project/Main.java
@@ -3,20 +3,7 @@ package project;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("ART");
-
-		System.out.println("Test");
-		for(int i = 0;i<10;i++) {
-			System.out.println("kuy");
-		}
-
-		System.out.println("TON");
-
-
-		System.out.println("Eufa");
-
+		UI.main(args);
 	}
 
 }
---
+++ src/project/UI.java
@@ -0,0 +1,51 @@
+package project;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class UI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI window = new UI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel = new JLabel("Test UI");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frame.getContentPane().add(lblNewLabel);
+	}
+
+}
