aab5e526ebeaaef5d124e677e0103e9505a510f7
ggneteufa <112540745+ggneteufa@users.noreply.github.com>
2023-02-27 14:17:55 +0700

    constucture table booking

--- src/project/Table_booking.java
+++ src/project/Table_booking.java
@@ -1,6 +1,10 @@
 package project;
 
 public class Table_booking extends Table_ID {
+	public Table_booking(String iD, String table_Status) {
+		super(iD, table_Status);
+		// TODO Auto-generated constructor stub
+	}
 	private Account register;
 	private String time;
 	private String phone_num;
