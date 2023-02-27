7e53a181f48408d1d526a6c54037cfc48d1990f5
ggneteufa <112540745+ggneteufa@users.noreply.github.com>
2023-02-27 14:24:23 +0700

    account

--- src/project/Account.java
+++ src/project/Account.java
@@ -1,13 +1,5 @@
 package project;
 
 public class Account {
-	private String Username;
-	private String password;
 	
-	public static void cal{
-		System.out.println("tt");
-	}
-	public static void main(String[]arg) {
-		
-	}
 }
